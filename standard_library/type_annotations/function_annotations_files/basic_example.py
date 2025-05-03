0b6653d3beae890e5ab11e4c0f1144009ae5d0b2
fedor <kobfedsur@gmail.com>
2025-05-03 23:32:21 +0300

    delete deprecated example files in the annotations

--- standard_library/type_annotations/function_annotations_files/basic_example.py
+++
@@ -1,4 +0,0 @@
-def some_function(arg1: int) -> float:
-    return arg1 / 2
-
-res: str = some_function(3.)
--- standard_library/type_annotations/function_annotations_files/none_return.py
+++
@@ -1,8 +0,0 @@
-def annotated_function() -> None:
-    print("I annotated to return None")
-
-def empty_function():
-    print("My output doesn't annotated")
-
-my_value = annotated_function()
-my_value = empty_function()
