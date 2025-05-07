67fadbb150e3984b1ca670b0432694c92e5fdd0a
fedor <kobfedsur@gmail.com>
2025-05-07 22:10:05 +0300

    add sqlite3.Connection as valid type fo execute_several_statement

--- src/sqlite.py
+++ src/sqlite.py
@@ -30,7 +30,7 @@ def split_sql_statement(code: str) -> list[str]:
 
 @typechecked
 def execute_several_statements(
-    cursor: sqlite3.Cursor,
+    cursor: sqlite3.Cursor | sqlite3.Connection,
     queries: str | list[str]
 ) -> list[tuple[Any, Any]]:
     """
