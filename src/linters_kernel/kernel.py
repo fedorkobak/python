fd0761bcecc4b2a218ba76a70a54e875d4a4d0c9
fedorkobak <kobfedsur@gmail.com>
2025-11-22 23:05:43 +0300

    add kernel class

---
+++ src/linters_kernel/kernel.py
@@ -0,0 +1,30 @@
+import os
+from tempfile import mkstemp
+from command_kernel import CommandKernel, command
+
+
+class LintersKernel(CommandKernel):
+    """
+    Kernel that apples a linter to the given code.
+    """
+
+    def _temp_file(self, code: str) -> str:
+        """
+        Create a tempfile that stores the code to be linted.
+
+        Returns
+        -------
+        Path to the file.
+        """
+        fd, path = mkstemp()
+        os.write(fd, code.encode("utf-8"))
+        os.close(fd)
+        return path
+
+    @command("# mypy")
+    def mypy(self, code: str) -> str:
+        return f"mypy {self._temp_file(code)}"
+
+    @command("# pyright")
+    def pyright(self, code: str) -> str:
+        return f"pyright {self._temp_file(code)}"
